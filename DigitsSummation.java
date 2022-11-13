217d0648ec2cf192ac6265da73fa10d2f6b89324
Ahmed Abd El-Monem <112562093+Amon3m@users.noreply.github.com>
2022-11-13 18:08:39 +0200

    Add files via upload

---
+++ DigitsSummation.java
@@ -0,0 +1,23 @@
+package oob;
+
+import java.util.Scanner;
+
+public class DigitsSummation {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.println("Enter number ");
+       int n = input.nextInt();
+        int digits=0;
+        int sum=0;
+
+        while (n!=0) {
+            digits=n% 10;
+            n/=10;
+              sum +=digits;
+          }
+        System.out.println("sum of digits is "+sum);
+
+
+    }
+    }
+
